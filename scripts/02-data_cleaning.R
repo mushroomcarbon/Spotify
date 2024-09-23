203abaa8bd4d1e7cb3a1ce32d3089e01f91766ed
Andrew Goh <andrew.goh@mail.utoronto.ca>
2024-09-23 02:46:30 +0000

    update cleaned data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -7,20 +7,24 @@
 # Pre-requisites: Need to have downloaded the data
 # Any other information needed? None.
 
-#### Workspace setup ####
-library(tidyverse)
+library(here)
+library(lubridate)
 
-#### Clean data ####
-raw_data <- read_csv("data/raw_data/raw_data.csv")
+# Read the raw data
+subwayData <- read.csv(here::here("./data/raw_data/raw_data.csv"))
+busData <- read.csv(here::here("./data/raw_data/raw_data_bus.csv"))
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |> 
-  separate(col = time_period,
-            into = c("year", "month"),
-            sep = "-") |> 
-  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-"))
-         )
-  
-#### Save data ####
-write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
+# Convert the Time column to a proper time format
+subwayData$Time <- hm(subwayData$Time)  # Parse 'Time' as hours and minutes
+busData$Time <- hm(busData$Time)
+
+# Extract the hour from the Time column
+subwayData$Hour <- hour(subwayData$Time)
+busData$Hour <- hour(busData$Time)
+
+# Create the directory for analysis data if it doesn't exist
+dir.create(here::here("./data/analysis_data"), showWarnings = FALSE)
+
+# Write the cleaned data to new CSV files
+write.csv(subwayData, here::here("./data/analysis_data/analysis_data.csv"), row.names = FALSE)
+write.csv(busData, here::here("./data/analysis_data/analysis_data_bus.csv"), row.names = FALSE)
