9630d7699d6a5b4285c4c864fcd01e25ff677829
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-10-10 21:09:47 +0000

    added test code for plots (need to delete)

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -24,3 +24,50 @@ is.double(data$energy)
 is.double(data$instrumentalness)
 is.double(data$valence)
 
+
+
+# plot of danceability over time
+ggplot(data, aes(x = album_release_year, y = danceability)) +
+  geom_point(alpha = 0.7, color = "black") + 
+  stat_summary(fun = mean, geom = "line", color = "green", size = 1.2) + 
+  labs(
+    title = "Danceability Over Album Release Years",
+    x = "Album Release Year",
+    y = "Danceability"
+  ) +
+  theme_minimal()
+
+# plot of energy over time
+ggplot(data, aes(x = album_release_year, y = energy)) +
+  geom_point(alpha = 0.7, color = "black") +
+  stat_summary(fun = mean, geom = "line", color = "blue", size = 1.2) +
+  labs(
+    title = "Energy Over Album Release Years",
+    x = "Album Release Year",
+    y = "Energy"
+  ) +
+  theme_minimal()
+
+# plot of instrumentalness over time
+ggplot(data, aes(x = album_release_year, y = instrumentalness)) +
+  geom_point(alpha = 0.7, color = "black") +
+  stat_summary(fun = mean, geom = "line", color = "red", size = 1.2) +
+  labs(
+    title = "Instrumentalness Over Album Release Years",
+    x = "Album Release Year",
+    y = "Instrumentalness"
+  ) +
+  theme_minimal()
+
+# plot of valence over time
+ggplot(data, aes(x = album_release_year, y = valence)) +
+  geom_point(alpha = 0.7, color = "black") +
+  stat_summary(fun = mean, geom = "line", color = "orange", size = 1.2) +
+  labs(
+    title = "Valence Over Album Release Years",
+    x = "Album Release Year",
+    y = "Valence"
+  ) +
+  theme_minimal()
+
+
