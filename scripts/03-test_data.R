70cb53c3636fc70aa6ea08f31db086b75c44b7ca
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-10-10 20:39:22 +0000

    added relevant tests on Spotify dataset

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Sanity check of the data
 # Author: Andrew Goh
-# Date: 19 September 2024
+# Date: 10 October 2024
 # Contact: andrew.goh@utoronto.ca
 # License: MIT
 # Pre-requisites: Need to have simulated data
@@ -10,109 +10,17 @@
 
 #### Workspace setup ####
 library(tidyverse)
-library(testthat)
 
-# test simulated data
-data <- read_csv(here::here("data/raw_data/simulated.csv"))
+# test data from Spotify
+data <- read_rds(here::here("data/the_national.rds"))
 
-# Convert Date column to Date format
-data$Date <- as.Date(data$Date)
+# test for missing values in the dataset
+any(is.na(data))
 
-# Test 1: Check if all dates are within 2022-2023
-test_that("All dates are within 2022-2023", {
-  expect_true(all(data$Date >= as.Date("2022-01-01") & data$Date <= as.Date("2023-12-31")))
-})
+# test whether variables of interest are doubles
+is.double(data$album_release_year)
+is.double(data$danceability)
+is.double(data$energy)
+is.double(data$instrumentalness)
+is.double(data$valence)
 
-# Test 2: Check if all Min.Delay values are non-negative
-test_that("No negative values in Min.Delay", {
-  expect_true(all(data$Min.Delay >= 0))
-})
-
-# Test 3: Check for any missing values in the data
-test_that("No missing values (NA) in the dataset", {
-  expect_false(any(is.na(data)))
-})
-
-# test raw data
-data <- read_csv(here::here("data/raw_data/raw_data.csv"))
-
-# Convert Date column to Date format
-data$Date <- as.Date(data$Date)
-
-# Test 1: Check if all dates are within 2022-2023
-test_that("All dates are within 2022-2023", {
-  expect_true(all(data$Date >= as.Date("2022-01-01") & data$Date <= as.Date("2023-12-31")))
-})
-
-# Test 2: Check if all Min.Delay values are non-negative
-test_that("No negative values in Min.Delay", {
-  expect_true(all(data$"Min Delay" >= 0))
-})
-
-# Test 3: Check for any missing values in the data
-test_that("No missing values (NA) in the dataset", {
-  expect_false(any(is.na(data)))
-})
-
-# test raw data for buses
-data <- read_csv(here::here("data/raw_data/raw_data_bus.csv"))
-
-# Convert Date column to Date format
-data$Date <- as.Date(data$Date)
-
-# Test 1: Check if all dates are within 2022-2023
-test_that("All dates are within 2022-2023", {
-  expect_true(all(data$Date >= as.Date("2022-01-01") & data$Date <= as.Date("2023-12-31")))
-})
-
-# Test 2: Check if all Min.Delay values are non-negative
-test_that("No negative values in Min.Delay", {
-  expect_true(all(data$"Min Delay" >= 0))
-})
-
-# Test 3: Check for any missing values in the data
-test_that("No missing values (NA) in the dataset", {
-  expect_false(any(is.na(data)))
-})
-
-# test cleaned data
-data <- read_csv(here::here("data/analysis_data/analysis_data.csv"))
-
-# Convert Date column to Date format
-data$Date <- as.Date(data$Date)
-
-# Test 1: Check if all dates are within 2022-2023
-test_that("All dates are within 2022-2023", {
-  expect_true(all(data$Date >= as.Date("2022-01-01") & data$Date <= as.Date("2023-12-31")))
-})
-
-# Test 2: Check if all Min.Delay values are non-negative
-test_that("No negative values in Min.Delay", {
-  expect_true(all(data$Min.Delay >= 0))
-})
-
-# Test 3: Check for any missing values in the data
-test_that("No missing values (NA) in the dataset", {
-  expect_false(any(is.na(data)))
-})
-
-# test cleaned data for buses
-data <- read_csv(here::here("data/analysis_data/analysis_data_bus.csv"))
-
-# Convert Date column to Date format
-data$Date <- as.Date(data$Date)
-
-# Test 1: Check if all dates are within 2022-2023
-test_that("All dates are within 2022-2023", {
-  expect_true(all(data$Date >= as.Date("2022-01-01") & data$Date <= as.Date("2023-12-31")))
-})
-
-# Test 2: Check if all Min.Delay values are non-negative
-test_that("No negative values in Min.Delay", {
-  expect_true(all(data$Min.Delay >= 0))
-})
-
-# Test 3: Check for any missing values in the data
-test_that("No missing values (NA) in the dataset", {
-  expect_false(any(is.na(data)))
-})
