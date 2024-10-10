c4932098b9528dbd36e241589c08246651b521bc
YisuHou1 <yisu.hou@mail.utoronto.ca>
2024-10-10 20:38:41 +0000

    edited date

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,14 +1,16 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from Spotify
 # Author: Andrew Goh
-# Date: 11 October 2024
+# Date: 10 October 2024
 # Contact: andrew.goh@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
 # Any other information needed? None
-
+library(spotifyr)
 library(dplyr)
 library(tidyverse)
 
 the_national <- get_artist_audio_features("the national")
-saveRDS(the_national, "data/raw_data/the_national.rds")
+saveRDS(the_national, "data/the_national.rds")
+
+
